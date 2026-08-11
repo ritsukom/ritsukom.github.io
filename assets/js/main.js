f1facca86234c64663aa0af26c78bab6ebd9fb35
ritsukom <ritsuko.m@mac.com>
2026-08-11 17:14:01 +0900

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+const toggle=document.querySelector('.menu-toggle');const nav=document.querySelector('.global-nav');if(toggle&&nav){toggle.addEventListener('click',()=>{const open=nav.classList.toggle('open');toggle.setAttribute('aria-expanded',String(open));});}
